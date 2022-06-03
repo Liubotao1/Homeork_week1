e98102533007025aed7a8a7fa33e3dc67b6b3c02
Liubotao1 <18392616412@163.com>
2022-06-03 17:50:13 +0800

    Initial commit

---
+++ src/Bubble_sort.java
@@ -0,0 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bubble_sort {
+    public static void main(String[] args) {
+        char k;
+        //创建对象
+        Scanner sc = new Scanner(System.in);
+        //接收数据
+        System.out.println("请输入字符串：");
+        String a = sc.next();
+
+        char[] ch = a.toCharArray();//将字符串存入数组
+        int length = a.length();//获取字符串长度
+        //System.out.println("有序字符串："+ length);
+
+        //冒泡排序法
+        for (short i = 0; i < length; i++) {
+            for (short j = 0; j < length-1; j++) {
+                if ((int) ch[j] < (int) ch[j+1]) {
+                    k = ch[j];
+                    ch[j] = ch[j + 1];
+                    ch[j + 1] = k;
+                }
+            }
+        }
+
+        //输出顺序字符串
+        String b = String.copyValueOf(ch); // 将数组转换为字符串
+        System.out.println(b);
+
+        //输出顺序字符串的ASCII码
+        int[] ascii = new int[length];
+        for (short i = 0; i < length; i++){
+            ascii[i] =(int)ch[i];
+        }
+        System.out.println("ASCII码："+ Arrays.toString(ascii));
+
+        sc.close();// 凡是使用IO流的类如果不关闭会一直占用资源，用完关闭。
+    }
+
+}
+
